6858be701cc43c6a23d19e76f1cb1e29e5d6af95
cybo12 <bohy.jeancyril@gmail.com>
2019-02-08 16:29:05 +0100

    process to bit optimisation for analog read

--- main/main.ino
+++ main/main.ino
@@ -18,51 +18,96 @@
 */
 
 #include <EEPROM.h>
+#include <math.h>
 
 #define DEBUG True
 
-int VERSION = 1;
-int UID_ADD = 0;
-int UID = 1;
-int payload_size = 3;
+uint8_t VERSION = 1;
+uint8_t UID = 1;
+uint8_t NB_SENS_A = 6;
+
+uint8_t UID_ADD = 0;
+uint8_t NB_BYTES_SENS_A = uint8_t(ceil(NB_SENS_A*10/8));
+uint8_t payload_size = 3+NB_BYTES_SENS_A;
 
 void setup() {
+  analogReference(INTERNAL);
   #ifdef DEBUG
   Serial.begin(9600);
   while(!Serial);
-  int value = EEPROM.read(UID_ADD);
-  if(value == 0){
+  if(UID != EEPROM.read(UID_ADD)){
       EEPROM.write(UID_ADD, UID );
       Serial.println("UID Never initalised");
     }
   Serial.print("UID: ");
-  Serial.println(value);
+  Serial.println(UID);
   Serial.print("Version: ");
   Serial.println(VERSION);
   #endif
 }
 
 void loop() {
-  delay(1000);
+  delay(10000);
   byte payload[payload_size];
-  int sensorValue = analogRead(A0);
-  float voltage = sensorValue / 1023.0;
   packeting(payload);
   #ifdef DEBUG
-  //Serial.println(voltage);
   for(int i = 0; i < payload_size; i++)
   {
+    Serial.print("byte ");
+    Serial.print(i);
+    Serial.print(": ");
     Serial.println(payload[i]);
   }
   #endif
 }
 
-void packeting(byte Payload[]){
-  Payload[0] = (byte) EEPROM.read(UID_ADD);
-  int battery = (int) 80/3;
-  byte byte_2 = (byte) battery << 3;
-  byte_2 = byte_2 | (byte) VERSION;
-  Payload[1] = (byte) VERSION;
-  Payload[2] = byte_2;
-
+void packeting(byte payload[]){
+  uint16_t battery = 13.12 *100;//TODO
+  byte byte_2 = (byte) battery;
+  byte byte_3 = (byte) battery << 8;
+  byte_3 = byte_3 | (byte) VERSION;
+  //initalise the payload
+  payload[0] = (byte) VERSION;
+  payload[1] = byte_2;
+  payload[2] = byte_3;
+  boolean values[NB_SENS_A*10];
+  byte addToPayload[NB_BYTES_SENS_A];
+  uint16_t valueRead;
+  uint8_t z = 0;
+  //make a array of bits from analog read (values going from 2 bytes to 10 bits)
+  for(uint8_t i = 0; i < NB_SENS_A; i++)
+    {
+      valueRead = analogRead(i);
+      Serial.print("A");
+      Serial.print(i);
+      Serial.print(": ");
+      Serial.print(valueRead);
+      Serial.print(" = ");
+      Serial.println(valueRead,BIN);
+      for(uint8_t x = 0; x<10;x++){
+        if(true){
+        values[z] = bitRead(valueRead,x);
+        }else{
+          values[z] = 0;
+        }
+        z++;
+      }
+    }
+  z=0;
+  //check array of bits
+  for(uint8_t p = 0; p<60;p++){Serial.print(values[p]);}
+  Serial.println();
+  //convert array of bits to array of bytes gain 25% of size. Gain= 2bit/byte
+  for(uint8_t y=0; y<NB_BYTES_SENS_A;y++)
+  {
+    for(uint8_t b=0; b<8; b++){
+      if(y*8+b < NB_SENS_A*10){
+        addToPayload[y] |= values[b] << b;
+      }else{
+        addToPayload[y] |= 0 << b;
+      }
+    }
+  }
+ //concate payload and array of bytes 
+memcpy(payload+3,addToPayload,sizeof(addToPayload));
 }
